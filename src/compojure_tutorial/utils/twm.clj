e642bd82863d6f2eabfe246ec96fcfde676f2c5b
Gabe Jensen <thegabejensen@gmail.com>
2024-03-15 07:29:57 -0700

    Got things into a workable state

--- src/compojure_tutorial/utils/twm.clj
+++ src/compojure_tutorial/utils/twm.clj
@@ -1,54 +1,49 @@
 (ns compojure-tutorial.utils.twm
-  (:require [clojure.string :as str]
-            [compojure-tutorial.utils.coll :refer [find-index]]))
+  (:require [clojure.string :as str]))
 
 ;;
 ;; CONSTS
 ;;
 
-(def CSS-PROPERTIES ["w",
-                     "max-w",
-                     "min-w",
-                     "m",
-                     "mt",
-                     "ml",
-                     "mr",
-                     "mb",
-                     "my",
-                     "mx",
-                     "p",
-                     "pt",
-                     "pl",
-                     "pr",
-                     "pb",
-                     "py",
-                     "px",
-                     "text",
-                     "bg",
-                     "rounded",
-                     "animate",
-                     "duration",
-                     "outline",
-                     "border",
-                     "cursor"])
-(def CSS-PROPERTY-PREFIXES (map #(str % "-") CSS-PROPERTIES))
+;; We might use these later
+
+;; (def CSS-PROPERTIES ["w",
+;;                      "max-w",
+;;                      "min-w",
+;;                      "m",
+;;                      "mt",
+;;                      "ml",
+;;                      "mr",
+;;                      "mb",
+;;                      "my",
+;;                      "mx",
+;;                      "p",
+;;                      "pt",
+;;                      "pl",
+;;                      "pr",
+;;                      "pb",
+;;                      "py",
+;;                      "px",
+;;                      "text",
+;;                      "bg",
+;;                      "rounded",
+;;                      "animate",
+;;                      "duration",
+;;                      "outline",
+;;                      "border",
+;;                      "cursor"])
+
+;; For testing purposes 
+
+;; (def EXAMPLE-CSS-MAP {"w" {"value" "w-5"
+;;                            "hover" {"value" "hover:w-10"}}
+;;                       "m" {"value" "m-3"}
+;;                       "p" {"value" "p-2"}})
+
 
 ;;
-;; FUNCTIONS
+;; EXAMPLE DATA
 ;;
-
-(defn match-css-property
-  [class]
-  (let [index (find-index #(str/starts-with? class %) CSS-PROPERTY-PREFIXES)
-        css-prop (get CSS-PROPERTIES index)]
-    css-prop))
-
-(defn css-map-to-coll
-  "Given a map of css properties/modifiers to classes, 
-   return the final css class string"
-  [class-map]
-  (println "TODO"))
-
 
 ;; CSS INPUT: 'text-green-700 hover:text-yellow-500 md:hover:text-red-500'
 
@@ -65,7 +60,31 @@
 ;;   }
 ;; }
 
-(defn get-modifiers
+
+;;
+;; FUNCTIONS
+;;
+
+(defn- recursive-get-css
+  [m css-str]
+  (reduce (fn [acc [k, v]] (if (= k "value")
+                             (str/trim (str acc " " v))
+                             (str/trim (str acc " " (recursive-get-css v css-str)))))
+          css-str
+          m))
+
+(defn- css-map-to-str
+  "Given a map of css properties/modifiers to classes, 
+   return the final css class string"
+  [css-map]
+  (println css-map)
+  (str/trim
+   (reduce (fn [acc css-str]
+             (str acc " " (str/trim css-str)))
+           ""
+           (map (fn [key] (recursive-get-css (get css-map key) "")) (keys css-map)))))
+
+(defn- get-modifiers
   "Given a string css class (e.g. `md:hover:text-blue-100`),
    return a collection of the modifiers in alphabetical order
    (e.g. ['hover', 'md'])"
@@ -75,48 +94,51 @@
       modifiers)
     '()))
 
-(defn parse-css-property
+(defn- parse-css-property
   "Given a css class, return just the css property itself
-   without modifiers (e.g. `md:hover:`) or spacing 
-   (e.g. `-[20px]` or `-24`)"
+   without modifiers (e.g. remove `md:hover:` in front) and without spacing 
+   (e.g. `-[20px]` or `-24` at the end)"
   [class]
   ;; Example class: `md:hover:w-24`
   (let [class-without-modifier (last (str/split class #":"))
         class-without-spacing (first (str/split class-without-modifier #"-"))]
     class-without-spacing))
 
-(defn css-reducer
-  "Given a css map, add in the appropriate keys/value of the additional css class"
+(defn- css-reducer
+  "Given a css map, add in the appropriate keys and 
+   final value of the given css class"
   [css-map class]
   (let [css-property (parse-css-property class)
         modifiers (get-modifiers class)
         map-path (conj modifiers css-property)]
     (assoc-in css-map map-path {"value" class})))
 
-(defn css-coll-to-map
+(defn- css-coll-to-map
   "Given a collection of css classes, return a map with 
    root-level keys as css classes, nested keys as modifiers, 
    and values as the classes themselves"
-  [css-coll]
-  (reduce css-reducer {} css-coll))
+  ([css-coll] (css-coll-to-map css-coll {}))
+  ([css-coll init-map]
+   (reduce css-reducer init-map css-coll)))
 
-(defn css-str-to-coll
+(defn- css-str-to-coll
+  "Given a string of css classes, return a collection of each individual class"
   [css-str]
   (sort (str/split css-str #" ")))
 
-(defn css-to-map
+(defn- css-to-map
   "Given a css string of several css classes, return a map
    representing the css structure"
-  [css-str]
-  (let [css-coll (css-str-to-coll css-str)
-        css-map (css-coll-to-map css-coll)]
-    css-map))
+  ([css-str] (css-to-map css-str {}))
+  ([css-str init-map]
+   (let [css-coll (css-str-to-coll css-str)
+         css-map (css-coll-to-map css-coll init-map)]
+     css-map)))
 
 (defn twm
   "Override default tailwind classes with new tailwind classes"
   [orig-css override-css]
   (let [orig-css-map (css-to-map orig-css)
-        override-css-map (css-to-map override-css)]
-    {"orig" orig-css-map, "override" override-css-map}))
-
-(twm "w-4" "w-6 mb-8 hover:mb-10")
+        override-css-map (css-to-map override-css orig-css-map)
+        new-css (css-map-to-str override-css-map)]
+    new-css))
